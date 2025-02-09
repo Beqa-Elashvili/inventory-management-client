1596f5ca7f32c818c9f0accf77a90d1f8a5415e4
Beqael <beqaelashvili3@gmail.com>
2025-02-09 21:56:24 +0400

    fix

--- .eslintrc.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rules: {
-    "@next/next/no-img-element": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "warn", // Change error to a warning
-  },
-};
--- src/app/(components)/Navbar/Navbar.tsx
+++ src/app/(components)/Navbar/Navbar.tsx
@@ -61,7 +61,7 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            <img src="logo2.jpg" className="w-9 h-9 rounded-full " />
+            <img alt="logo" src="logo2.jpg" className="w-9 h-9 rounded-full " />
           </div>
           <span className="font-semibold">ADMIN</span>
         </div>
